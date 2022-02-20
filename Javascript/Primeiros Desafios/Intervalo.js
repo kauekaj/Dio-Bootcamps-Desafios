34d2d0b0431b2e83ac7f5f7688ffd65be4285206
Kauê Jacyntho <kauekaj@gmail.com>
2022-02-20 15:26:37 -0300

    Update Intervalo.js

--- Javascript/Primeiros Desafios/Intervalo.js
+++ Javascript/Primeiros Desafios/Intervalo.js
@@ -18,17 +18,21 @@ A saída deve ser uma mensagem conforme exemplo abaixo.
 */
 
 
-let X = parseFloat(gets());
+// let X = parseFloat(gets());
 
-if (X>=0 && X<=25){print("Intervalo [0,25]" );
+// if (X>=0 && X<=25){print("Intervalo [0,25]" );
 
-}else if(X > 25 && X <= 50){print("Intervalo (25,50]");
+// }else if(X > 25 && X <= 50){print("Intervalo (25,50]");
 
-}else if(X > 50 && X<=  75){print("Intervalo (50,75]");
+// }else if(X > 50 && X<=  75){print("Intervalo (50,75]");
 
-}else if(X > 75 && X<=  100){print("Intervalo (75,100]"); 
+// }else if(X > 75 && X<=  100){print("Intervalo (75,100]"); 
 
-}else{print("Fora de intervalo");
+// }else{print("Fora de intervalo");
 
-}
+// }
 
+let x = 5;
+let y = 2;
+let z = x/y;
+console.log(++z);
